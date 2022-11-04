413bf6e95394c980e7395a06505ddb131dfde23b
Seungwon Jo <89008053+frontjsw@users.noreply.github.com>
2022-11-04 14:50:11 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,7 @@
 function solution(n, k) {
     var answer = 0;
     const sheep = 12000;
-    const cock = 2000;
-    answer = sheep*n + cock*k - Math.floor(n / 10) * cock; 
+    const drink = 2000;
+    answer = sheep*n + drink*k - Math.floor(n / 10) * drink; 
     return answer;
 }
