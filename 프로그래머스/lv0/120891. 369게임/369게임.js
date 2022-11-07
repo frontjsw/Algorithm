12371b9112c311c962fc9d2d1fb0c6c0436ac517
Seungwon Jo <89008053+frontjsw@users.noreply.github.com>
2022-11-07 16:23:33 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,10 @@
+function solution(order) {
+    const arr = order.toString().split('');
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === "3" || arr[i] === "6" || arr[i] === "9"){
+            count += 1;
+        }
+    }
+    return count;
+}
